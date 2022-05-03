6040a9066df2e34928e2342f41c30194128c1dd6
Bryan Waring <94076324+Bryanc15@users.noreply.github.com>
2022-05-02 20:44:56 -0400

    Create for_loops.sh

---
+++ Linux/for_loops.sh
@@ -0,0 +1,4 @@
+#!/bin/bash nums=$(echo {0..9})states=('Nebraska' 'California' 'Texas' 'Hawaii' 'Washington'ls_out=$(ls)
+execs=$(find /home -type f -perm 777 2>/dev/null)
+for num in ${nums[@]}; doif [ $num = 3 ] || [ $num = 5 ] || [ $num = 7 ]; thenecho $numfidone
+for state in ${states[@]}; doif [ $state == 'Hawaii' ]; thenecho "Hawaii is the best!"elseecho "NO vacation for the instructorfidonefor x in ${ls_out[@]}; do echo $xType  :qa!  and press <E...all changes and exit Vim     
